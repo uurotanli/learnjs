28f6805cb6d2524eb5f1e7f96ff05b262bb22f33
Taguchi <rtaguchi@abeam.com>
2017-08-22 22:15:23 +0900

    ch3まで完了

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -14,4 +14,50 @@ describe('LearnJS', function(){
         learnjs.showView('#problem-42');
         expect(learnjs.problemView).toHaveBeenCalledWith('42');
     });
+
+    it('invokes the router when loaded', function(){
+        spyOn(learnjs, 'showView');
+        learnjs.appOnReady();
+        expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash);
+    }); 
+
+    it('subscribes to the hash change event', function(){
+        learnjs.appOnReady();
+        spyOn(learnjs, 'showView');
+        $(window).trigger('hashchange');
+        expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash);
+    });
+
+    describe('problem view', function(){
+        it('has a title that includes the problem number', function(){
+            var view = learnjs.problemView('1');
+            expect(view.find('.title').text()).toEqual('Problem #1')
+        });
+        it('shows the description', function(){
+            var view = learnjs.problemView('1');
+            expect(view.find('[data-name="description"]').text()).toEqual('What is truth?');
+        });
+        it('shows the problem code', function(){
+            var view = learnjs.problemView('1');
+            expect(view.find('[data-name="code"]').text()).toEqual('function problem(){ return __;}');
+        });
+    });
+
+    describe('answer section', function(){
+        var view;
+        beforeEach(function(){
+            view = learnjs.problemView('1');
+        });
+
+        // it('can check a correct answer by hitting a button', function(){
+        //     view.find('.answer').val('true');
+        //     view.find('.check-btn').click();
+        //     expect(view.find('.result').text()).toEqual('Correct!');
+        // });
+        it('rejects an incorrect answer', function(){
+            view.find('.answer').val('false');
+            view.find('.check-btn').click();
+            expect(view.find('.result').text()).toEqual('Incorrect!');
+        });
+    });
 });
